a2f9e8398882f9f0c484855c812d8665385ec1d0
twelfthdoctor1 <officialtwelfthdoctor1@hotmail.com>
2022-05-31 23:08:09 +0800

    Added commenting

--- Plotter.py
+++ Plotter.py
@@ -5,22 +5,28 @@ import numpy as np
 import math
 import time
 
+# Data Housing
 ACC_DATA = []
 ACC_X = []
 ACC_Y = []
 ACC_Z = []
-ACC_POINTS = ""
 TIME_AXIS = []
 
-CYCLES = 0
-MAX_CYCLES = 50
+# Cycles
+# Set Max Cycles to a specified number, e.g. if you want 10 cycles, put 10
+CYCLES = 0  # <- Cycle Count, DO NOT TOUCH VALUE
+MAX_CYCLES = 50  # <- Set the number of cycles for data to be collected
 
 # Serial Config
-# Serial Port is name of the Serial port, check VSCode for connected port
+# Serial Port is name of the Serial port, check VSCode or Arduino IDE for connected port
 # Baud Rate is the Serial Baud Rate used in Arduino
 SERIAL_PORT = "/dev/cu.usbserial-0001"
 BAUD_RATE = 9600
 
+# NOTE!
+# Before launching the code, check that you have specified the correct Serial Port and Baud Rate
+# When launching the code, there is a chance that the code will fail due to incomplete serial data which breaks decoding
+# As such, re-run the code until a non-Exception output comes up.
 
 try:
     ard_serial = serial.Serial(SERIAL_PORT, baudrate=BAUD_RATE)
@@ -42,6 +48,7 @@ while True:
         # Acceleration of 3 Axis Combined
         acc_comb = math.sqrt(pow(float(ser_data[1]), 2) + pow(float(ser_data[2]), 2) + pow(float(ser_data[3]), 2))
 
+        # Data Append to form List for Arrays
         ACC_DATA.append(acc_comb)
         ACC_X.append(ser_data[1])
         ACC_Y.append(ser_data[2])
@@ -60,7 +67,7 @@ while True:
             for i in range(len(ACC_DATA)):
                 TIME_AXIS.append(i)
 
-            # Show Plot
+            # Plot and Show Graph
             #plot.plot(TIME_AXIS, acc_x, TIME_AXIS, acc_y, TIME_AXIS, acc_z)
             plot.plot(acc_x, color="r")
             plot.plot(acc_y, color="g")
