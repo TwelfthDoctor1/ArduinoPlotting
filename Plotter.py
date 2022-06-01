e5b3cf909bd80bccec5e0ea6d3f94f9c5c3e9a1d
twelfthdoctor1 <officialtwelfthdoctor1@hotmail.com>
2022-06-01 11:49:14 +0800

    Update code

--- Plotter.py
+++ Plotter.py
@@ -1,9 +1,40 @@
+#  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  TwelfthDoctor1's Arduino Plotting
+#
+#  > 2D Plotter
+#
+#  (C) Copyright TD1 & TWoCC 2022
+#  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  Licensed under MIT License
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#
+#  Codes from other parties are not part of the License and Copyright.
+#  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
 import serial
 import sys
 import matplotlib.pyplot as plot
 import numpy as np
 import math
 import time
+from MasterApprenticeLib.TD1_Lib_MasterLogger import MasterLogger
 
 # Data Housing
 ACC_DATA = []
@@ -23,30 +54,59 @@ MAX_CYCLES = 50  # <- Set the number of cycles for data to be collected
 SERIAL_PORT = "/dev/cu.usbserial-0001"
 BAUD_RATE = 9600
 
+# Logging
+master_logger = MasterLogger(
+    "2D Plotter",
+    main_owner="TwelfthDoctor1",
+    additional_context="2D Plotter Graph"
+)
+
 # NOTE!
 # Before launching the code, check that you have specified the correct Serial Port and Baud Rate
 # When launching the code, there is a chance that the code will fail due to incomplete serial data which breaks decoding
 # As such, re-run the code until a non-Exception output comes up.
 
+# Try to establish Serial Connection
 try:
+    print(f"Initiate Serial Connection at {SERIAL_PORT} at {BAUD_RATE} baud.")
+
     ard_serial = serial.Serial(SERIAL_PORT, baudrate=BAUD_RATE)
 
+# Except if Serial is in use or no device is connected
 except serial.SerialException as e:
     print(f"[ERROR]: PORT IS IN USE OR DEVICE IS NOT CONNECTED! STOP OTHER SERIAL PROCESSES AND TRY AGAIN.\n\n{e}")
 
-    sys.exit()
+    sys.exit()  # Stop code
 
 # cycles = eval(input("Enter the number of cycles for data to be collected to then be plotted: "))
 
 while True:
+    # Decoding from bytes to string
+    # Unstable Concurrently
     ser_ln = ard_serial.readline()[:-2].decode("utf-8")
 
     if ser_ln.startswith("-> PY"):
+        # In ser_data, there are 8 items in the list.
+        # Acceleration takes 1 to 3
+        # Gyroscope takes 4 to 6
+        # Temperature takes 7
         ser_data = ser_ln.split("|")
-        print(ser_data)
+        # print(ser_data)
 
         # Acceleration of 3 Axis Combined
-        acc_comb = math.sqrt(pow(float(ser_data[1]), 2) + pow(float(ser_data[2]), 2) + pow(float(ser_data[3]), 2))
+        acc_comb = math.sqrt(pow(float(ser_data[1]), 2) + pow(float(ser_data[2]), 2) + pow(float(ser_data[3]), 2))  # - 8
+
+        # Print Out On Console
+        print("================================================================================================")
+        print(f"[Acceleration] -> X: {ser_data[1]} | Y: {ser_data[2]} | Z: {ser_data[3]} | Combined: {acc_comb}")
+        print(f"[Rotation] -> X: {ser_data[4]} | Y: {ser_data[5]} | Z: {ser_data[6]}")
+        print(f"[Temperature] -> {ser_data[7]} degC")
+
+        master_logger.log(
+            f"[Acceleration] -> X: {ser_data[1]} | Y: {ser_data[2]} | Z: {ser_data[3]} | Combined: {acc_comb}\n"
+            f"[Rotation] -> X: {ser_data[4]} | Y: {ser_data[5]} | Z: {ser_data[6]}\n"
+            f"[Temperature] -> {ser_data[7]} degC"
+        )
 
         # Data Append to form List for Arrays
         ACC_DATA.append(acc_comb)
@@ -69,14 +129,15 @@ while True:
 
             # Plot and Show Graph
             #plot.plot(TIME_AXIS, acc_x, TIME_AXIS, acc_y, TIME_AXIS, acc_z)
-            plot.plot(acc_x, color="r")
-            plot.plot(acc_y, color="g")
-            plot.plot(acc_z, color="b")
+            plot.plot(acc_points, color="orange")  # 3 axis Acceleration
+            #plot.plot(acc_x, color="r")  # X Axis
+            #plot.plot(acc_y, color="g")  # Y Axis
+            #plot.plot(acc_z, color="b")  # Z Axis
 
             plot.xlabel("Cycles")
             plot.ylabel("Acceleration (ms-2)")
             plot.grid()
-            plot.legend(["X", "Y", "Z"])
+            plot.legend(["Combined", "X", "Y", "Z"])
 
             plot.show()
             break
